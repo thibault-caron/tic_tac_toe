6b4371e47c4a982afc1b0630ab253c01d0e131c5
Thibault Caron <thibault.caronvans@laposte.net>
2024-12-02 08:55:21 +0100

    added game board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -9,3 +9,18 @@ else:
     player1 = "O"
 
 print(f"Player 1 is {player1} and Player 2 is {player2}")
+
+
+# board array
+board = [" " for x in range(9)]
+
+def printBoard():
+    print("     |     |      ")
+    print("  " + board[0] + "  |  " + board[1] + "  |  " + board[2] + " ")
+    print("_____|_____|_____    1 | 2 | 3 ")
+    print("     |     |       -------------")
+    print("  " + board[3] + "  |  " + board[4] + "  |  " + board[5] + "      4 | 5 | 6 ")
+    print("_____|_____|_____  -------------")
+    print("     |     |         7 | 8 | 9 ")
+    print("  " + board[6] + "  |  " + board[7] + "  |  " + board[8] + "  ")
+    print("     |     |     ")
